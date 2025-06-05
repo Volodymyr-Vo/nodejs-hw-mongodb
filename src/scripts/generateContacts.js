700d8f17ec1caad7ca5994b1f3bbcadb66fffdf3
Volodymyr_Strilchuk <woha@ukr.net>
2025-06-05 14:13:24 +0300

    hw7

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,12 @@
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number = 5) => {
+  const contacts = await readContacts();
+  const newContacts = Array.from({ length: number }, () => createFakeContact());
+  const updatedContacts = [...contacts, ...newContacts];
+  await writeContacts(updatedContacts);
+};
+
+generateContacts();
